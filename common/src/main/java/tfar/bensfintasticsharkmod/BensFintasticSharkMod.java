2be85c9ca99df305dae6ad1c01cb5fefbf7310ae
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-05-21 03:14:44 -0400

    boilerplate

---
+++ common/src/main/java/tfar/bensfintasticsharkmod/BensFintasticSharkMod.java
@@ -0,0 +1,14 @@
+package tfar.bensfintasticsharkmod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BensFintasticSharkMod {
+
+    public static final String MOD_ID = "bensfintasticsharkmod";
+    public static final String MOD_NAME = "BensFintasticSharkMod";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+
+    public static void init() {
+    }
+}
--- forge/src/main/java/tfar/bensfintasticsharkmod/BensFintasticSharkModForge.java
+++ forge/src/main/java/tfar/bensfintasticsharkmod/BensFintasticSharkModForge.java
@@ -1,19 +1,19 @@
-package com.example.examplemod;
+package tfar.bensfintasticsharkmod;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(CommonClass.MOD_ID)
-public class ExampleMod {
+@Mod(BensFintasticSharkMod.MOD_ID)
+public class BensFintasticSharkModForge {
     
-    public ExampleMod() {
+    public BensFintasticSharkModForge() {
     
         // This method is invoked by the Forge mod loader when it is ready
         // to load your mod. You can access Forge and Common code in this
         // project.
     
         // Use Forge to bootstrap the Common mod.
-        CommonClass.LOG.info("Hello Forge world!");
-        CommonClass.init();
+        BensFintasticSharkMod.LOG.info("Hello Forge world!");
+        BensFintasticSharkMod.init();
         
     }
 }
